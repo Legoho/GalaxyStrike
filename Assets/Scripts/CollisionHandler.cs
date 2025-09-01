54016f28f28b17a5d6affd72025437e82a65fe53
Legoho <almos.szirmai@gmail.com>
2025-09-01 10:40:51 +0200

    Hitpoints and multiple enemies

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] GameObject deathVFX;
     private void OnTriggerEnter(Collider other)
     {
+        Instantiate(deathVFX, transform.position, Quaternion.identity);
         Debug.Log("Collision detected with: " + other.gameObject.name);
+        Destroy(this.gameObject);
     }
 }
--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,10 +4,16 @@ public class Enemy : MonoBehaviour
 {
 
     [SerializeField] GameObject deathVFX;
+    [SerializeField] int hitPoints = 10;
     private void OnParticleCollision(GameObject other)
     {
-        Instantiate(deathVFX, transform.position, Quaternion.identity);
-        Destroy(gameObject);
+        hitPoints--;
+        if (hitPoints <= 0)
+        {
+            Instantiate(deathVFX, transform.position, Quaternion.identity);
+            Destroy(gameObject);
+
+        }
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
